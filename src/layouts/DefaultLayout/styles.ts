3517195c5b429f15c7bd112196ea65a0a0947ee8
Enick Jhony <enick.jhony@gmail.com>
2023-08-15 22:21:54 -0400

    Add `DefaultLayout` and `Header`

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  margin: 2rem 10rem;
+
+  display: flex;
+  flex-direction: column;
+`
