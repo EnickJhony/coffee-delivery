f029d91cffcbf65703760ba2359efaafe7b043c0
Enick Jhony <enick.jhony@gmail.com>
2023-08-19 22:01:26 -0400

    Add ListCoffee at `Home`

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,37 @@
 import {
+  Cart,
   CartItem,
+  CoffeeActions,
+  CoffeeCard,
+  CoffeeDescription,
   CoffeeItem,
+  CoffeeList,
+  CoffeePrice,
+  CoffeeTag,
+  CoffeeTitle,
+  CoffeeValue,
+  Counter,
+  Currency,
   HomeContainer,
   InfoContent,
   InfoImg,
   InfoItem,
   InfoItems,
+  OursCoffee,
   PackageItem,
+  Price,
   TimerItem,
 } from "./styles";
+import Expresso from "../../assets/Type=Expresso.png";
 import CoffeImg from "../../assets/CoffeImg.png";
-import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+import {
+  Coffee,
+  Minus,
+  Package,
+  Plus,
+  ShoppingCart,
+  Timer,
+} from "phosphor-react";
 
 export function Home() {
   return (
@@ -55,6 +76,179 @@ export function Home() {
           alt="Copo de Café fechado com grãos de café ao redor"
         />
       </InfoContent>
+      <OursCoffee>
+        <h2>Nossos Cafés</h2>
+        <CoffeeList>
+          <CoffeeCard>
+            <img src={Expresso} alt="Xícara de Expresso" />
+            <CoffeeTag>Tradicional</CoffeeTag>
+            <CoffeeTitle>Expresso tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O Tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+            <CoffeePrice>
+              <CoffeeValue>
+                <Currency>R$</Currency>
+                <Price>9,90</Price>
+              </CoffeeValue>
+              <CoffeeActions>
+                <Counter>
+                  <Minus size={16} weight="bold" color="#8047F8" />
+                  <span>1</span>
+                  <Plus size={16} weight="bold" color="#8047F8" />
+                </Counter>
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" color="#FFF" />
+                </Cart>
+              </CoffeeActions>
+            </CoffeePrice>
+          </CoffeeCard>
+          <CoffeeCard>
+            <img src={Expresso} alt="Xícara de Expresso" />
+            <CoffeeTag>Tradicional</CoffeeTag>
+            <CoffeeTitle>Expresso tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O Tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+            <CoffeePrice>
+              <CoffeeValue>
+                <Currency>R$</Currency>
+                <Price>9,90</Price>
+              </CoffeeValue>
+              <CoffeeActions>
+                <Counter>
+                  <Minus size={16} weight="bold" color="#8047F8" />
+                  <span>1</span>
+                  <Plus size={16} weight="bold" color="#8047F8" />
+                </Counter>
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" color="#FFF" />
+                </Cart>
+              </CoffeeActions>
+            </CoffeePrice>
+          </CoffeeCard>
+          <CoffeeCard>
+            <img src={Expresso} alt="Xícara de Expresso" />
+            <CoffeeTag>Tradicional</CoffeeTag>
+            <CoffeeTitle>Expresso tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O Tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+            <CoffeePrice>
+              <CoffeeValue>
+                <Currency>R$</Currency>
+                <Price>9,90</Price>
+              </CoffeeValue>
+              <CoffeeActions>
+                <Counter>
+                  <Minus size={16} weight="bold" color="#8047F8" />
+                  <span>1</span>
+                  <Plus size={16} weight="bold" color="#8047F8" />
+                </Counter>
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" color="#FFF" />
+                </Cart>
+              </CoffeeActions>
+            </CoffeePrice>
+          </CoffeeCard>
+          <CoffeeCard>
+            <img src={Expresso} alt="Xícara de Expresso" />
+            <CoffeeTag>Tradicional</CoffeeTag>
+            <CoffeeTitle>Expresso tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O Tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+            <CoffeePrice>
+              <CoffeeValue>
+                <Currency>R$</Currency>
+                <Price>9,90</Price>
+              </CoffeeValue>
+              <CoffeeActions>
+                <Counter>
+                  <Minus size={16} weight="bold" color="#8047F8" />
+                  <span>1</span>
+                  <Plus size={16} weight="bold" color="#8047F8" />
+                </Counter>
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" color="#FFF" />
+                </Cart>
+              </CoffeeActions>
+            </CoffeePrice>
+          </CoffeeCard>
+          <CoffeeCard>
+            <img src={Expresso} alt="Xícara de Expresso" />
+            <CoffeeTag>Tradicional</CoffeeTag>
+            <CoffeeTitle>Expresso tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O Tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+            <CoffeePrice>
+              <CoffeeValue>
+                <Currency>R$</Currency>
+                <Price>9,90</Price>
+              </CoffeeValue>
+              <CoffeeActions>
+                <Counter>
+                  <Minus size={16} weight="bold" color="#8047F8" />
+                  <span>1</span>
+                  <Plus size={16} weight="bold" color="#8047F8" />
+                </Counter>
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" color="#FFF" />
+                </Cart>
+              </CoffeeActions>
+            </CoffeePrice>
+          </CoffeeCard>
+          <CoffeeCard>
+            <img src={Expresso} alt="Xícara de Expresso" />
+            <CoffeeTag>Tradicional</CoffeeTag>
+            <CoffeeTitle>Expresso tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O Tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+            <CoffeePrice>
+              <CoffeeValue>
+                <Currency>R$</Currency>
+                <Price>9,90</Price>
+              </CoffeeValue>
+              <CoffeeActions>
+                <Counter>
+                  <Minus size={16} weight="bold" color="#8047F8" />
+                  <span>1</span>
+                  <Plus size={16} weight="bold" color="#8047F8" />
+                </Counter>
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" color="#FFF" />
+                </Cart>
+              </CoffeeActions>
+            </CoffeePrice>
+          </CoffeeCard>
+          <CoffeeCard>
+            <img src={Expresso} alt="Xícara de Expresso" />
+            <CoffeeTag>Tradicional</CoffeeTag>
+            <CoffeeTitle>Expresso tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O Tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+            <CoffeePrice>
+              <CoffeeValue>
+                <Currency>R$</Currency>
+                <Price>9,90</Price>
+              </CoffeeValue>
+              <CoffeeActions>
+                <Counter>
+                  <Minus size={16} weight="bold" color="#8047F8" />
+                  <span>1</span>
+                  <Plus size={16} weight="bold" color="#8047F8" />
+                </Counter>
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" color="#FFF" />
+                </Cart>
+              </CoffeeActions>
+            </CoffeePrice>
+          </CoffeeCard>
+        </CoffeeList>
+      </OursCoffee>
     </HomeContainer>
   );
 }
