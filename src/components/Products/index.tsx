d103bf7b987dbe9beee36b6fe57f5a46990bc749
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-12-04 22:33:10 -0400

    Separando o `Card` da listagem dos produtos

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,66 +1,15 @@
-import {
-  Cart,
-  CoffeeActions,
-  CoffeeCard,
-  CoffeeDescription,
-  CoffeeList,
-  CoffeePrice,
-  CoffeeTag,
-  CoffeeTitle,
-  CoffeeValue,
-  Counter,
-  Currency,
-  OursCoffee,
-  Price,
-} from "./styles";
-import { Minus, Plus, ShoppingCart } from "phosphor-react";
-
 import { Product } from "../../stock/products";
-import { useState } from "react";
+import { Card } from "../Card";
+import { CoffeeCard, CoffeeList, OursCoffee } from "./styles";
 
 export function Products() {
-  const [quantity, setQuantity] = useState(1);
-
-  const handleIncreaseQuantity = (): void => {
-    setQuantity((state) => state + 1);
-  }
-
-  const handleDecreaseQuantity = (): void => {
-    setQuantity((state) =>  state === 1 ? state : state - 1);
-  }
-
   return (
     <OursCoffee>
       <h2>Nossos Cafés</h2>
       <CoffeeList>
         {Product.map((item) => (
           <CoffeeCard key={item.id}>
-            <img src={item.img} alt={item.title} />
-            <CoffeeTag>Tradicional</CoffeeTag>
-            <CoffeeTitle>{item.title}</CoffeeTitle>
-            <CoffeeDescription>{item.description}</CoffeeDescription>
-            <CoffeePrice>
-              <CoffeeValue>
-                <Currency>R$</Currency>
-                <Price>{item.price}</Price>
-              </CoffeeValue>
-              <CoffeeActions>
-                <Counter>
-                  <button onClick={handleDecreaseQuantity}>
-                    <Minus size={16} weight="bold" color="#8047F8" />
-                  </button>
-                  <span>{quantity}</span>
-                  <button onClick={handleIncreaseQuantity}>
-                    <Plus size={16} weight="bold" color="#8047F8" />
-                  </button>
-                </Counter>
-                <Cart>
-                  <button>
-                    <ShoppingCart size={22} weight="fill" color="#FFF" />
-                  </button>
-                </Cart>
-              </CoffeeActions>
-            </CoffeePrice>
+            <Card item={item} />
           </CoffeeCard>
         ))}
       </CoffeeList>
